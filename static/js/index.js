989056526b24a203896b4ce81d520c33705bb920
Mauricio <mdpilatuna.fie@unach.edu.ec>
2020-09-08 13:50:37 -0500

    edit5

--- static/js/index.js
+++ static/js/index.js
@@ -1,34 +1,31 @@
 //https://www.eclipse.org/paho/clients/js/
 
-function ALTO() {
-    //alert("led on");
-    console.log("led on");
-    //document.getElementById("sensor").innerHTML="led on";
-    message =new Paho.MQTT.Message("LED1_ALTO");
+function Encender1() {
+    message =new Paho.MQTT.Message("L1O");
     message.destinationName = "mdpilatuna.fie@unach.edu.ec/repaso";
     client.send(message);
  
 }
-function BAJO(){   
-    //alert("led off");
-    console.log("led off");
-    //document.getElementById("sensor").innerHTML="led off";
-    message =new Paho.MQTT.Message("LED1_bajo");
+function Apagar1(){   
+    message =new Paho.MQTT.Message("L1N");
+    message.destinationName = "mdpilatuna.fie@unach.edu.ec/repaso";
+    client.send(message);
+}
+function Encender2() {
+    message =new Paho.MQTT.Message("L2O");
+    message.destinationName = "mdpilatuna.fie@unach.edu.ec/repaso";
+    client.send(message);
+ 
+}
+function Apagar2(){   
+    message =new Paho.MQTT.Message("L2N");
     message.destinationName = "mdpilatuna.fie@unach.edu.ec/repaso";
     client.send(message);
 }
 
 
-
-
-
-
-// Create a client instance
-  //client = new Paho.MQTT.Client("postman.cloudmqtt.com", 14970);
-  
   client = new Paho.MQTT.Client("maqiatto.com", 8883, "web_" + parseInt(Math.random() * 100, 10));
 
-  // set callback handlers
   client.onConnectionLost = onConnectionLost;
   client.onMessageArrived = onMessageArrived;
   var options = {
